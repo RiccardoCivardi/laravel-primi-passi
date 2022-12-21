9184686fde69cbe357f52ff6daff7a164463186d
RiccardoCivardi <riccardo.civardi@icloud.com>
2022-12-21 16:21:46 +0100

    [add] css

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -4,12 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+
     <title>Landing - Laravel primi passi</title>
+
 </head>
 <body>
 
-    <h1>{{$title}}</h1>
-    <a href="/">{{$link_text}}</a>
+    <div class="container">
+
+        <h1>{{$title}}</h1>
+        <a href="/">{{$link_text}}</a>
+
+    </div>
 
 </body>
 </html>
